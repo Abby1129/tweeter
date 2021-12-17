589a41d0f8fff84aae7287fffae9069f97c0b8e2
Abby <abby_faigao@yahoo.com>
2021-12-17 07:16:36 +0000

    Initial commit

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  const $textarea = $("#tweet-textarea");
+  const $counter = $(".counter");
+  const tweetLength = 140;
+
+  $textarea.on("keyup", function () {
+    $counter.text(tweetLength - $textarea.val().length);
+    if ($counter.text() < 0) {
+      $counter.css("color", "red");
+    } else {
+      $counter.css("color", "black");
+    }
+  });
+});
