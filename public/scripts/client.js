e8288c84e0f8e129f7e1fdb3840d9a6eb890cbcc
Abby <abby_faigao@yahoo.com>
2021-12-22 01:14:21 +0000

    Make dynamic elements

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,68 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
+    },
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
+    },
+    created_at: 1461116232227,
+  },
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
+    },
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
+  },
+];
+
+$(document).ready(function () {
+  const renderTweets = function (tweets) {
+    const $tweetContainer = $(".tweet-container");
+
+    for (const tweet of tweets) {
+      $tweetContainer.append(createTweetElement(tweet));
+    }
+  };
+
+  const createTweetElement = function (tweetData) {
+    const $tweet = $(`
+    <section id="tweet-container">
+    <article>
+    <header>
+    <div class="tweet-img-name">
+    <img src ="${tweetData.user.avatars}"/>
+    <h1>${tweetData.user.name}</h1>
+    </div>
+    <div class="tweet-username">
+    <h2>${tweetData.user.handle}</h2>
+    </div>
+    </header>
+    <div class="tweet-content"> 
+    <p>${tweetData.content.text}.</p> </div>
+    <footer>
+    <h3>${tweetData.created_at}</h3>
+    <div class="footer-icons">
+    <i class="fas fa-flag"></i>
+    <i class="fas fa-retweet"></i>
+    <i class="fas fa-heart"></i>
+    </div>
+    </footer>
+    </article>
+    </section>
+    `);
+    return $tweet;
+  };
+
+  renderTweets(data);
+});
