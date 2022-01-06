70fcb21f8ce17a54a4403ee9e56b9cdb8d357ad5
Abby <abby_faigao@yahoo.com>
2022-01-06 00:30:05 +0000

    More clean up and double check element names

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -20,34 +20,34 @@ $(document).ready(function () {
   // CREATE NEW TWEET ELEMENT
   const createTweetElement = function (tweetData) {
     return `
-      <article>
-        <header>
-          <div class="tweet-img-name">
-            <img src ="${escape(tweetData.user.avatars)}"/>
-            <h1>${escape(tweetData.user.name)}</h1>
-          </div>
-          <div class="tweet-username">
-            <h2>${escape(tweetData.user.handle)}</h2>
-          </div>
-        </header>
+    <article>
+    <header>
+      <div class="tweet-img-name">
+        <img src ="${escape(tweetData.user.avatars)}"/>
+        <h1>${escape(tweetData.user.name)}</h1>
+      </div>
+      <div class="tweet-username">
+        <h2>${escape(tweetData.user.handle)}</h2>
+      </div>
+    </header>
 
-        <div class="tweet-content">
-          <p>${escape(tweetData.content.text)}</p>
-        </div>
+    <div class="tweet-content">
+      <p>${escape(tweetData.content.text)}</p>
+    </div>
 
-        <footer>
-          <h3>${timeago.format(escape(tweetData.created_at))}</h3>
-          <div class="footer-icons">
-            <i class="fas fa-flag"></i>
-            <i class="fas fa-retweet"></i>
-            <i class="fas fa-heart"></i>
-          </div>
-        </footer>
-      </article>
+    <footer>
+      <h3>${timeago.format(escape(tweetData.created_at))}</h3>
+      <div class="footer-icons">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>
+    </footer>
+  </article>
     `;
   };
 
-  $(".form-new-tweet").on("submit", function (event) {
+  $(".new-tweet").on("submit", function (event) {
     errorDiv.slideUp(80);
     event.preventDefault();
     const form = this;
